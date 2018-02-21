3c7dae8d01ef4405a5aed0f1258d8b2f0c235880
Santi Teeragul <santitee@gmail.com>
2018-02-21 17:29:33 +0700

    CREATED `GoogleMapAPI` Component

---
+++ src/components/services/API_GoogleMap.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class GoogleMapAPI extends Component {
+    constructor (props) {
+        super(props)
+    }
+    getLocation() {
+
+    }
+}
