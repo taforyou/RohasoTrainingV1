4dada7a17862a527cbaa1a01c43691d4943a668e
jimmyis <jimmy.israkhan@gmail.com>
2018-02-26 08:39:43 +0700

    UPDATED `App` component with map get coordinates functionality and styles.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Container } from 'native-base'
+import MapView , { Marker } from 'react-native-maps'
 
 export default class App extends Component {
 	constructor() {
@@ -11,17 +12,58 @@ export default class App extends Component {
 				longitude: 100.5382926,
 				latitudeDelta: 0,
 				longitudeDelta: 0
-			}
+			},
+			mapType: "standard",
+			currentPlace: null,
 		}
+		this.onRegionChange = this.onRegionChange.bind(this)
+		this.mapPressed = this.onMapPressed.bind(this)
+	}
+	onRegionChange(region) {
+		// console.log('Latitude: ' + region.latitude + ' Longitude:' + region.longitude)
+		setTimeout(function () {
+			this.setState({
+				mapCoordinates: {
+						latitude: region.latitude,
+						longitude: region.longitude,
+						latitudeDelta: region.latitudeDelta,
+						longitudeDelta: region.longitudeDelta,
+				}
+			})
+		}.bind(this), 2000);
+	}
+	onMapPressed(value) {
 	}
 	render() {
 		return (
-			<View>
-				<MapView
-					style={{width: 400, height: 400}}
-					region={this.state.mapCoordinates}
-				/>
-			</View>
-		);
+			<Container style={{ flex: 1, backgroundColor: '#ffffff' }}>
+				<TouchableOpacity
+					activeOpacity={0.9}
+					style={styles.mapView}
+				>
+					<MapView
+						style={styles.map}
+						provider="google"
+						mapType={this.state.mapType}
+						loadingEnabled={true}
+						initialRegion={this.state.mapCoordinates}
+						onRegionChangeComplete={this.onRegionChange}
+					/>
+				</TouchableOpacity>
+				<Text>
+					Latitude: {this.state.mapCoordinates.latitude} Longitude: {this.state.mapCoordinates.longitude}
+				</Text>
+			</Container>
+		)
 	}
 }
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+	},
+	mapView: {
+		flex: 0,
+		height: '60%'
+	}
+});
