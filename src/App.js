125dd9899bc78112a6efe6056a981e934cf74b97
jimmyis <jimmy.israkhan@gmail.com>
2018-02-26 07:03:43 +0700

    ADDED internal state to `App` component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import { View } from 'react-native';
 import MapView from 'react-native-maps';
 
 export default class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			mapCoordinates: {
+				latitude: 13.7649276,
+				longitude: 100.5382926,
+				latitudeDelta: 0,
+				longitudeDelta: 0
+			}
+		}
+	}
 	render() {
 		return (
 			<View>
 				<MapView
 					style={{width: 400, height: 400}}
-					region={{
-						latitude: 13.7649276,
-						longitude: 100.5382926,
-						latitudeDelta: 0,
-						longitudeDelta: 0
-					}}
+					region={this.state.mapCoordinates}
 				/>
 			</View>
 		);
