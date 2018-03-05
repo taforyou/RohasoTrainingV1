635d371ca63d3d624402732e682a978a232d90cd
jimmyis <jimmy.israkhan@gmail.com>
2018-03-06 06:55:19 +0700

    REARCHITECTED the project, Used App component as a root of important state and method (Globalized).

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
-import React from 'react'
-import { StackNavigator } from 'react-navigation'
-import Main from './components/Main'
+import React, { Component } from 'react';
+import { View, Text, Alert } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import MainPage from './components/Main';
+import MapPage from './components/Map';
 
-export default App = StackNavigator({
-  Main: {
-    screen: Main,
-  },
-})
+const Stack = StackNavigator(
+	{
+		Main: { screen: MainPage },
+		Map: { screen: MapPage }
+	},
+	{
+		initialRouteName: 'Main',
+	}
+);
+
+export default class App extends Component {
+	constructor(props){
+		super(props);
+		console.log("App @ constructor()")
+		this.state = {
+			currentLocation: {
+				coords: {
+					latitude: 0,
+					longitude: 0,
+					latitudeDelta: 0,
+					longitudeDelta: 0,
+				},
+				address: [ {
+					formatted_address: ''
+				} ],
+			},
+			userLocation: {
+				coords: {
+					latitude: 0,
+					longitude: 0,
+					latitudeDelta: 0,
+					longitudeDelta: 0,
+				},
+				address: [ {
+					formatted_address: ''
+				} ],
+			},
+			senderLocation: {
+				coords: {
+					latitude: 0,
+					longitude: 0,
+					latitudeDelta: 0,
+					longitudeDelta: 0,
+				},
+				address: [ {
+					formatted_address: ''
+				} ],
+			},
+			receiverLocation: {
+				coords: {
+					latitude: 0,
+					longitude: 0,
+					latitudeDelta: 0,
+					longitudeDelta: 0,
+				},
+				address: [ {
+					formatted_address: ''
+				} ],
+			},
+		}
+		this.rootMethod = {
+		}
+	};
+	// Methods to pass as props : bind
+
+	// Methods internally use
+
+	// Component Life-Cycles
+
+	render() {
+		return (
+			<Stack screenProps={{ rootMethod: this.rootMethod, rootState: this.state }}/>
+		);
+	}
+}
