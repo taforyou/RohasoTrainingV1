e53cf4a6ca73f27d09b0c8bcb0bff9fbbfb973e0
jimmyis <jimmy.israkhan@gmail.com>
2018-03-06 15:44:27 +0700

    ADDED React component life-cycle debugging marker in `Main` page component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,50 @@
-import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import * as NB from 'native-base'
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import * as NB from 'native-base';
 
 export default class Main extends Component {
 	constructor(props) {
 		super(props);
+		console.log("Main Comp. @ constructor()");
 		this.props = props;
 		this.root = {
 			state: props.screenProps.rootState,
 			method: props.screenProps.rootMethod,
-		}
-	}
+		};
+	};
 	static navigationOptions = {
     header: null,
-  };
+	};
+	// Component Life Cycles
+	componentWillMount() {
+		console.log('Main Comp. @ componentWillMount');
+	};
+	componentDidMount() {
+		console.log('Main Comp. @ componentDidMount');
+	};
+	componentWillReceiveProps(props) {
+		console.log('Main Comp. @ componentWillReceiveProps');
+			if(debug) {
+				console.log('-----= props =-----'); console.log(props);
+			};
+	};
+	componentWillUpdate(nextProps, nextState) {
+		console.log('Main Comp. @ componentWillUpdate');
+			if(debug) {
+				console.log('-----= nextProps =-----'); console.log(nextProps);
+				console.log('-----= nextState =-----'); console.log(nextState);
+			};
+	};
+	componentDidUpdate(prevProps, prevState) {
+		console.log('Main Comp. @ componentDidUpdate')
+			if(debug) {
+				console.log('-----= prevProps =-----'); console.log(prevProps);
+				console.log('-----= prevState =-----'); console.log(prevState);
+			};
+	};
+	componentWillUnmount() {
+		console.log('Main Comp. @ componentWillUnmount');
+	};
 	render() {
 		return (
 			<NB.Container>
@@ -21,6 +52,6 @@ export default class Main extends Component {
 					<Text style={{ color: 'white'}}>Next</Text>
 				</NB.Button>
 			</NB.Container>
-		)
-	}
-}
+		);
+	};
+};
